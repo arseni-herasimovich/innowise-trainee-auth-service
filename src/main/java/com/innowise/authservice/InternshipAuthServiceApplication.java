2699084ebad4c570da4f1357c25b6a9ae4c6755b
arseni-herasimovich <arseni.herasimovich@innowise.com>
2025-10-31 23:23:17 +0300

    init commit

---
+++ src/main/java/com/innowise/authservice/InternshipAuthServiceApplication.java
@@ -0,0 +1,11 @@
+package com.innowise.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipAuthServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InternshipAuthServiceApplication.class, args);
+    }
+}
