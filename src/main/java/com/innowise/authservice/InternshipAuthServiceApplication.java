ca4e802f0b781788d370b65448cfc7b28ef33b95
arseni-herasimovich <arseni.herasimovich@innowise.com>
2025-11-12 14:07:55 +0300

    feat: enable discovery client

--- src/main/java/com/innowise/authservice/InternshipAuthServiceApplication.java
+++ src/main/java/com/innowise/authservice/InternshipAuthServiceApplication.java
@@ -2,8 +2,10 @@ package com.innowise.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InternshipAuthServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(InternshipAuthServiceApplication.class, args);
